15472ab31c5c5650882cf92ec1737a6c5a63975b
Marko <markogajiin@gmail.com>
2020-11-10 13:28:27 +0100

    Project stracture refactored.

--- common/mq/consumer.go
+++
@@ -1,9 +0,0 @@
-package mq
-
-// Message type is used for message handlers.
-type Message = interface{}
-
-// MessageHandler interface
-type MessageHandler interface {
-	HandleMessage(Message)
-}
--- main.go
+++ main.go
@@ -1,28 +1,3 @@
 package main
 
-import (
-	"github.com/mgajin/kpjm-scraper/app"
-)
-
-var done chan interface{}
-
-func init() {
-	done = make(chan interface{})
-}
-
-func main() {
-
-	a := app.NewApp()
-
-	go a.FetchCategories()
-
-	for _, controller := range a.StorageControllers {
-		go controller.Handle()
-	}
-
-	for _, controller := range a.CrawlerControllers {
-		go controller.Crawl()
-	}
-
-	<-done
-}
+func main() {}
