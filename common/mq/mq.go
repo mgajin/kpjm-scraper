466f0e660a3026779fd6aee24d62b9633167cc01
Marko <markogajiin@gmail.com>
2020-11-10 14:33:02 +0100

    MQ lib refactored.

--- common/mq/mq.go
+++ common/mq/mq.go
@@ -1,1 +1,16 @@
 package mq
+
+// Message type represents message that will be passed through queues and channels.
+type Message = interface{}
+
+// MessageHandler handles a message consumed
+// from the message queue.
+type MessageHandler interface {
+	HandleMessage(message Message)
+}
+
+// MessageBroker is used for consuming and publishing messages from and to message queue.
+type MessageBroker interface {
+	Consume() Message
+	Publish(message Message)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mgajin/kpjm-scraper
 
 go 1.15
+
+require github.com/valyala/fastjson v1.6.1 // indirect
